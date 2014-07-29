aa6a4d9630d6950b94699c5c55a06c64c5e5147c
hbb20 <harshbbhakta@gmail.com>
2014-07-29 17:08:57 +0530

    111 word added and redomization applied 	new file:   raw_meaning 	new file:   raw_word 	modified:   word_game.css 	modified:   word_game.html 	modified:   word_game.js

--- word_game.js
+++ word_game.js
@@ -1,5 +1,7 @@
-		var word_list=["apple","cigarette","steve","giraffe","cow"]; //Word list that will be comes
-		var mean_list=["A fruit,usually with red color....A fruit,usually with red color...A fruit,usually with red color....A fruit,usually with red color...A fruit,usually with red color","A small stick shaped object , used for smoking","Co-founder of Apple Inc.","An animal with tall nack","A holy animal for hindu, gives milk as well"];
+		var raw_word_list=["actor", "addition", "admire", "apple", "April", "august", "beautiful", "blue", "cabbage", "camera", "car", "carrot", "choice", "church", "cloth", "complex", "contact", "cook", "cow", "cricket", "crime", "daily", "December", "dentist", "doctor", "dollar", "door", "easy", "elephant", "eraser", "eye", "February", "Friday", "giraffe", "gun", "hair", "heaven", "hockey", "house", "January", "July", "June", "knife", "lamp", "lawyer", "leader", "march", "may", "member", "Monday", "monkey", "moon", "mother", "mountain", "museum", "noise", "notebook", "November", "occasion", "October", "office", "orange", "owner", "paper", "phone", "poison", "pollution", "poor", "purse", "quite", "relax", "request", "rice", "rope", "rush", "salad", "satisfaction", "Saturday", "school", "scissors", "score", "seek", "September", "shirt", "short", "shoulder", "signal", "soccer", "soldier", "square", "stream", "Sunday", "table", "television", "temple", "theatre", "thief", "thumb", "Thursday", "Tuesday", "umbrella", "unique", "vacation", "wallet", "watch", "water", "Wednesday", "whistle", "window", "worship", "Zebra"]; //Word list that will be comes
+		var word_list=[]; //Word list that will be comes
+		var raw_mean_list=["A one who act in drama","Summation of any thing","To praise something or somebody for work.","A rounded fruit with red color.","A month that comes after March and before  May.","A month that comes after July and before September.","Thing which is visibly nice ","A color of sky","A cultivated plant eaten as a vegetable, having thick green or purple leaves surrounding a spherical heart or head of young leaves.","An instrument used to capture the picture","A vehicle with four tyres","A tapering orange-coloured root eaten as a vegetable. ","Selection between two or more options","Holy place for Catholic people","A fibre made thing that can wear","Something which is not simple in usual way","A person or industrial body in touch","To prepare food with the use of fire","A holy animal for Hindu, gives milk as well","A game played with bat and ball","An unlawful act punishable by a state","Some action that is performed every day","Last month of the Year","Doctor that gives treatment of teeth related problem","The one who treat some sick person or animal","American currency","A place from where entry is taken to any building","Things that can be done with out any special efforts","A big animal with huge ear and trunk","Thing which can rubout the note written by pencil","Part of human body which enable a one to see around the world","A month that comes after January and before March","A day of week which comes after Thursday","An animal with tall neck","A weapon which fires bullet and shoots target","A filament that comes out from human body","A place which give pleasure of paradise","National game of India","A building where human live","First month of the Year","A month that comes after June and before August","A month that comes after May and before July","A tool that used to cut vegetables","A light source that gives low intense light","A person who has knowledge of law of Nation","A one who leads any team or political party or organization","A month that comes after February and before April","A month that comes after ","A person who is part of any team","The day of week comes after Sunday ","An animal that can jump from tree to tree","A natural satellite of earth, clearly visible at night ","Person who gave birth","A big piece or collection of rock, hard to climb","A place where old and precious things are kept and cared","The situation which created by unwanted sounds","A thing created by binding the blank papers together","A month which comes after October and before December","An event of celebration","A month that comes after September and before November","Work place of any organization","Name of color which is found on fruit of that name.","The person who owns the property or thing","A flat thin thing on which writing or printing is done","Electronic instrument which enables communication","Things which are unexpected and dangerous to human body","Situation generation due to Unwanted environmental activity","The person with less money","A container that is used to keep small important things","The place with no noise or voice","Sleep or do nothing after a period of activity ","To ask for something or ask somebody to do something in a polite or formal way","A cooking variety that with small white piece","Very strong thick string made by twisting thinner strings","A situation in which you are in a hurry and need to do things quickly","A mixture of raw vegetables","The good feeling that you have when you have achieved something ","A day of week that comes before Sunday","A place where Student goes for education","Tool for cutting paper or cloththat has two sharp blades with handlesjoined together in the middle","To win points goals etc. in a game or competition","To look for something","A month that comes after August and before October","A piece of clothing, worn on the upper part of the body","Object with less height","Either of the two parts of the body between the top of each arm and the neck","To make a movement or sound to give somebody a message, an order","A game played by hitting ball by foot","A member of an army","A shape with four same sides and same angels","A small narrow river","Day of week which comes before Monday","A piece of furniture that consists of a flat top supported by legs","Electrical equipment with a screen on which you can watch programmes with moving pictures and sounds","Holy place of Hindu where they go for prayer","A building or an outdoor area where plays and similar types of entertainment are performed","A person who steals something from another person or place","The short thick finger at the side of the hand, slightly apart from the other four","Day of Week that comes before Friday","Day of week that comes after Monday","An object with a round folding frame of long straight pieces of metal covered with material, that you use to protect yourself from the rain or from hot sun","Being the only one of its kind","Bunch of / collection  holidays","A small flat folding case made of leather or plastic used for keeping paper money and credit cards in","An electrical instrument that shows time","A liquid important for human body","A day of week that comes after Tuesday","To make a high sound or a musical tune by forcing your breath out when your lips are closed","A hole of home that allows exchange of air with outside","To go to a service in a religious building","An animal with black and white strips"];
+		var mean_list=[];
 		var number_of_tiles=9,	//Number of tiles in grid
 			current_character_number=0, 
 			current_word_number=0,
@@ -16,8 +18,50 @@
 			last_start_time=0,
 			is_hint_open=false;
 
+		
+		//this function will return array with suffled object in it..
+		function get_shuffled_indexes(upper_limit) {
+  			
+    			 var array=[];
+    			for(var i=0;i<upper_limit;i++) 
+    			{
+    				array[i]=i;
+    			}
+
+    			var currentIndex = array.length,
+    		 	temporaryValue, randomIndex;
+
+  				// While there remain elements to shuffle...
+  				while (0 !== currentIndex) {
+
+    			// Pick a remaining element...
+    			randomIndex = Math.floor(Math.random() * currentIndex);
+    			currentIndex -= 1;
+
+    			// And swap it with the current element.
+    			temporaryValue = array[currentIndex];
+    			array[currentIndex] = array[randomIndex];
+    			array[randomIndex] = temporaryValue;
+  				}
+  				console.log("random array of index is "+array);
+ 		 	return array;
+		}
+
+		//This function will load the words and meaning from raw array to actual array by using radomized index array
+		function randomize_words_means(){
+			var random_index=get_shuffled_indexes(raw_word_list.length);
+			var current_random_index;
+			for(var i=0;i<random_index.length;i++)
+			{
+				current_random_index=random_index[i];
+				word_list[i]=raw_word_list[current_random_index];
+				mean_list[i]=raw_mean_list[current_random_index];
+			}
+		}
+
 		//This function Suffle the characters within word.
 		//"hellow" will be "olwehl" or "lhowel" or anithing.
+
 		String.prototype.shuffle = function ()
 		{
 			var a = this.split(""),
@@ -122,7 +166,7 @@
             if (e.style.webkitAnimationName !== 'flip_tile') {
                 e.style.webkitAnimationName = 'flip_tile';
                 e.style.webkitAnimationDuration = time_req+'s';
-                
+                if(type!="btn_pause" && type!="btn_play")
                 e.style.background=flip_color;   
                 setTimeout(function() {
                    e.style.webkitAnimationName = '';
@@ -342,6 +386,7 @@
 		 		if(play_state==0)
 		 		{	
 		 			flip(e,"btn_play")
+		 			e.className=e.className.replace(" btn-success"," btn-warning");
 		 			e.innerHTML=' <font size="5px"><span class="glyphicon glyphicon-pause" ></span><font color="#2E2E2E">Pause</font></font>';
 		 			play_it();	
 		 			
@@ -349,6 +394,7 @@
 		 		else
 		 		{
 		 			flip(e,"btn_pause")
+		 			e.className=e.className.replace(" btn-warning"," btn-success");
 		 			e.innerHTML='<font size="5px"><span class="glyphicon glyphicon-play" ></span><font color="#2E2E2E">Play</font></font>';
 		 			pause_it();
 		 		}
@@ -384,7 +430,7 @@
 		 	if(play_state!=-1)
 		 	{
 			var e=document.getElementById("hint_place");
-			e.style.lineHeight="20px";
+			e.style.lineHeight="18px";
 		 	e.innerHTML='<font size="4px" color="silver"><p style="margin-bottom:5px;">Spell a word that stands for following meaning</font><br/></p><font size="5px" color="white">'+mean_list[current_word_number-1]+'</font><font color="#e1e1e1" size="4px"><br/><div id="show_hint_div class="show_word_box"> <button id="show_word_box" type="button" class="btn btn-warning btn-lg show_word_box" " onclick="show_hint(3);" onmouseover="show_hint(3)"><font size="4px><span class="glyphicon glyphicon-info-sign" ></span><font color="#2E2E2E">Find your word here...</font></font></button></div>';		 	
 		 	flip(e,"hint");
 
@@ -452,6 +498,7 @@
  		function start()
  		{	pause_timer();
  			total_score=0;
+ 			randomize_words_means();
  			var e=document.getElementById("div_rb_start");
  				e.disabled=true;
  				document.getElementById("quit_btn").disabled=false;
@@ -508,7 +555,7 @@
 					diff=diff-18;
 					bar=document.getElementById("bar_two");
 					document.getElementById("bar_three").style.width="28%";
-					percent=30*(18-diff)/18;
+					percent=30*(17-diff)/18;
 					bar.style.width=percent+"%";
 				}
 
